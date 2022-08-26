dbc40f9d277be63d5b3c776a9508aed4d4ce3521
verah <verahnyagaka101@gmail.com>
2022-08-26 20:37:22 +0300

    question 1 & 2

--- variables-and-data-types/app.rb
+++ variables-and-data-types/app.rb
@@ -2,10 +2,21 @@
 ## Q1 Set variables for the corresponding values:
 
 #a number to the value 25
+num_1 = 25
+puts num_1
+
 #a boolean to the value true 
+is_monday = true
+puts is_monday
+
 #a string to the value "Ruby"
+language = "Ruby"
+puts language
 
 ##  Q2 Set values 100, 30 for the  variables cars and drivers respectively.
 #Output: There are 100 cars and 30 drivers.
+cars = 100
+drivers = 30
+puts "There are #{cars} cars and #{drivers} drivers."
 
 
